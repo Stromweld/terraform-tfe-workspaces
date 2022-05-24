627b068e0391c6c42cbbdf6e7505e41b1bee33dc
Corey Hemminger <hemminger@hotmail.com>
2022-05-24 14:34:46 -0500

    initial commit (#1)

---
+++ test/variables.tf
@@ -0,0 +1,5 @@
+variable "tfc_token" {
+  type        = string
+  description = "Token to authenticate to TFC"
+  sensitive   = true
+}
---
+++ variables.tf
@@ -0,0 +1,41 @@
+variable "name" {
+  type        = string
+  description = "Name of workspace"
+}
+variable "description" {
+  type        = string
+  default     = null
+  description = "Description of workspace"
+}
+variable "organization" {
+  type        = string
+  default     = "Stromweld"
+  description = "Owning Org for workspace"
+}
+variable "terraform_version" {
+  type        = string
+  default     = null
+  description = "The version of Terraform to use for this workspace. Defaults to the latest available version. "
+}
+variable "vcs_repo" {
+  type = map(string)
+  default = {
+    enabled = false
+  }
+  description = "Used to add VCS repo to workspace"
+}
+variable "repo_identifier" {
+  type        = string
+  default     = null
+  description = "VCS identifier to link repo to"
+}
+variable "oauth_token_id" {
+  type        = string
+  default     = null
+  description = "Oauth Token for VCS provider configured in TFC"
+}
+variable "speculative_enabled" {
+  type        = bool
+  default     = true
+  description = "Enable speculative plans"
+}
